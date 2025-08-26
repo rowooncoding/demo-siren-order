5e9d3768f9fb9a71b24b8cfe7a7a23b86eea5aa0
Rowoon <dev.rowoon@gmail.com>
2025-08-26 14:01:03 +0900

    chore: init

---
+++ src/main/java/com/example/demosirenorder/DemoSirenOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosirenorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSirenOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSirenOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosirenorder/DemoSirenOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosirenorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSirenOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
