c19365422da9bfc4ebf615ab96eb3c428fd0cd53
Rowoon <dev.rowoon@gmail.com>
2025-09-03 11:39:53 +0900

    feat: List 자료형을 Set으로 변경

--- src/main/java/com/example/demosirenorder/domain/Order.java
+++ src/main/java/com/example/demosirenorder/domain/Order.java
@@ -8,10 +8,7 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Getter
 @Table(name = "orders")
@@ -26,17 +23,17 @@ public class Order {
     private Timestamp orderedAt;
 
     // 주문이 생성되면 그에 따른 오더 아이템들이 연계
-    @MappedCollection(idColumn = "order_item_id", keyColumn = "order_id")
-    private List<OrderItem> orderItems = new ArrayList<>();
+    @MappedCollection(idColumn = "order_id", keyColumn = "order_item_id")
+    private Set<OrderItem> orderItems = new HashSet<>();
 
-    public Order(int customerId, List<OrderItem> orderItems) {
+    public Order(int customerId, Set<OrderItem> orderItems) {
         this.customerId = customerId;
         this.orderedAt = Timestamp.valueOf(LocalDateTime.now());
         this.orderItems = orderItems;
     }
 
     public static Order newOrder(CreateOrder createOrder) {
-        List<OrderItem> items = new ArrayList<>();
+        Set<OrderItem> items = new HashSet<>();
 
         for (Map.Entry<Integer, Integer> entry : createOrder.getQuantityByProduct().entrySet()) {
             items.add(new OrderItem(entry.getKey(), entry.getValue()));
